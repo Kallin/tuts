58feeede09a0f7bdf8dbd3ab921749e527f9e3c5
kallin <kallin.nagelberg@gmail.com>
2015-04-19 19:09:06 -0400

    finished 2.3

--- game_dev_with_create_js/app/lib/main.js
+++ game_dev_with_create_js/app/lib/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var domReady = require('./util/dom_ready');
+var Hero = require('./Hero');
 
 var c = createjs;
 
@@ -9,48 +10,20 @@ console.log('Game Started!: EaselJS version: ' + c.EaselJS.version);
 domReady(function init() {
     var stage = new c.Stage('main');
 
-    var group = new c.Container();
-    group.x = 200;
-    group.y = 175;
-    group.regX = 100;
-    group.regY = 100;
-    stage.addChild(group);
+    var hero1 = new Hero('Hero 1', 50, 50);
+    stage.addChild(hero1);
 
-    group.on('tick', function (event) {
-        this.rotation -= -1;
-    });
+    var hero2 = new Hero('Hero 2', 150, 50);
+    stage.addChild(hero2);
 
-    var circle = new c.Shape();
-    circle.graphics.beginFill('green').drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    group.addChild(circle);
-
-    var square = new c.Shape();
-    square.graphics.beginFill('green')
-        .drawRect(0, 0, 50, 50)
-        .endFill()
-        .beginStroke("rgba(0,0,0,1)")
-        .moveTo(5, 75)
-        .bezierCurveTo(45, 90, 75, 75, -25, -25)
-        .endStroke();
-    square.x = 200;
-    square.y = 100;
-    group.addChild(square);
-
-
-    var introText = new c.Text('Welcome to the game.', '20px Arial', '#ff7700');
-    introText.x = 200;
-    introText.y = 300;
-    introText.lineWidth = 100;
-    stage.addChild(introText);
+    console.log(stage);
 
     c.Ticker.timingMode = c.Ticker.RAF;
     c.Ticker.setFPS(60);
     c.Ticker.addEventListener('tick', function(event) {
-        stage.update();
+        stage.update({
+            delta: event.delta
+        });
     });
-
-
 
 });
